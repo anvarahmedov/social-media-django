b7828fb8b02ea1de52c90b450e525214f270abef
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-29 10:19:04 +0500

    installed pillow

--- core/models.py
+++ core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Profile(models.Model):
-    user = pass
-    user_id = pass
-    bio = pass
+    #user = pass
+    #user_id = pass
+    #bio = pass
     profileimg = models.ImageField()
-    location = pass
+    #location = pass
