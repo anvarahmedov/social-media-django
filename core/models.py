4fe763bbe79ee3ecdd599f998b61d17340ea2a80
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-29 10:05:23 +0500

    modified settings file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = pass
+    user_id = pass
+    bio = pass
+    profileimg = models.ImageField()
+    location = pass
