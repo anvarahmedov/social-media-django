2fc25d0ef7d798f1e715e7db8128a9bb36e48f5b
anvarahmedov <boyoftokyo19@gmail.com>
2025-02-02 13:24:24 +0500

    implemented search

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
     path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('profile/<int:pk>/', views.profile, name='profile'),
     path('like-post/<str:post_id>/', views.like_post, name='like_post'),
     path('signup', views.signup, name='signup'),
